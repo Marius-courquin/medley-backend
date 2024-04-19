63a06064fe23543df06ac247a0d5b840191e180a
Marius <marius.courquin@ig2i.centralelille.fr>
2024-04-19 14:45:16 +0200

    fix(auth): db env variables values

--- core/src/app.module.ts
+++ core/src/app.module.ts
@@ -15,11 +15,11 @@ import * as process from "process";
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      host: process.env.CORE_DB_HOST,
+      port: parseInt(process.env.CORE_DB_PORT) || 5432,
+      username: process.env.CORE_DB_USER,
+      password: process.env.CORE_DB_PASSWORD,
+      database: process.env.CORE_DB_NAME,
       entities: [
         __dirname + '/domain/entities/*.entity{.ts,.js}',
       ],
