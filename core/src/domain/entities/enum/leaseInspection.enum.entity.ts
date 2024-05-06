2cf46f1884f9e23ba1a7e6a025cc415a59285971
Charles_Bournonville <charles25.bournonville@gmail.com>
2024-05-06 15:25:48 +0200

    temp

---
+++ core/src/domain/entities/enum/leaseInspection.enum.entity.ts
@@ -0,0 +1,10 @@
+export enum LeaseInspectionType {
+    CHECK_IN = "CHECK_IN",
+    CHECK_OUT = "CHECK_OUT",
+};
+
+export enum LeaseInspectionState {
+    PENDING = "PENDING",
+    IN_PROGRESS = "IN_PROGRESS",
+    DONE = "DONE",
+};
---
+++ core/src/domain/entities/leaseInspection.entity.ts
@@ -0,0 +1,40 @@
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, ManyToOne } from 'typeorm';
+import { IsDateString, IsEnum, IsOptional, IsUUID } from 'class-validator';
+import { LeaseInspectionType, LeaseInspectionState } from '@domain/entities/enum/leaseInspection.enum.entity';
+import { Lease } from '@domain/entities/lease.entity';
+
+@Entity()
+export class LeaseInspection {
+    @PrimaryGeneratedColumn("uuid")
+    @IsOptional()
+    @IsUUID(4, { message: 'id must be a valid uuid' })
+    id?: string;
+
+    @Column({nullable: false, type: 'enum', enum : LeaseInspectionType})
+    @IsEnum(LeaseInspectionType, { message: 'type must be a valid type' })
+    type: LeaseInspectionType;
+
+    @Column({nullable: false, type: 'enum', enum : LeaseInspectionState})
+    @IsEnum(LeaseInspectionState, { message: 'state must be a valid type' })
+    state: LeaseInspectionState;
+
+    @Column({nullable: false, name : "end_date"})
+    @IsDateString()
+    endDate: Date;
+
+    @ManyToOne(() => Lease, lease => lease.id, {nullable: true, eager: true})
+    @IsOptional()
+    lease?: Lease;
+
+    @CreateDateColumn({ type: "timestamp", default: () => "CURRENT_TIMESTAMP(6)" , name : "created_at"})
+    createdAt: Date;
+
+    constructor(type: LeaseInspectionType, state: LeaseInspectionState, endDate: Date, lease?: Lease, id?: string) {
+        this.id = id ?? undefined;
+        this.type = type;
+        this.state = state;
+        this.endDate = endDate;
+        this.lease = lease ?? undefined;
+    }
+
+}
