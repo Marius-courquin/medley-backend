7dc249f94aa4423aa0aa866101e5e55d8abe6158
Marius-courquin <marius.courquin@ig2i.centralelille.fr>
2024-05-10 12:12:00 +0200

    feat(auth): complete swagger documentation and update dependencies

--- auth/src/infrastructure/controllers/auth.controller.ts
+++ auth/src/infrastructure/controllers/auth.controller.ts
@@ -3,19 +3,94 @@ import {AuthService} from '@domain/services/auth.service';
 import {UserLoginDto} from "../dtos/userLogin.dto";
 import {UserSignupDto} from "@infrastructure/dtos/userSignup.dto";
 import {UserRevocationDto} from "@infrastructure/dtos/userRevocation.dto";
+import {
+    ApiBadRequestResponse,
+    ApiBody,
+    ApiNotFoundResponse,
+    ApiOkResponse,
+    ApiTags,
+    ApiUnauthorizedResponse
+} from "@nestjs/swagger";
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
     constructor(
         private service: AuthService
     ) {}
 
+    @ApiBody({ type: UserLoginDto })
+    @ApiNotFoundResponse(
+        {
+            description: 'User does not exist',
+            schema: {
+                example: {
+                    statusCode: 404,
+                    message: 'User does not exist',
+                },
+            }
+        }
+    )
+    @ApiUnauthorizedResponse(
+        {
+            description: 'Invalid credentials',
+            schema: {
+                example: {
+                    statusCode: 401,
+                    message: 'Invalid credentials',
+                },
+            }
+        }
+    )
+    @ApiOkResponse(
+        {
+            description: 'User logged in successfully',
+            schema: {
+                type: 'object',
+                properties: {
+                    access_token: {
+                        type: 'string',
+                        example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJtYWduaTI1IiwiaWQiOiJtYWduaTI1IiwidXNlcm5hbWUiOiJtYWduaTI1In0.7',
+                        description: 'The access token to be used in the Authorization header'
+
+                }
+            }
+        }
+    })
     @HttpCode(HttpStatus.OK)
     @Post('login')
     signIn(@Body() user: UserLoginDto) {
         return this.service.signIn(user);
     }
 
+    @ApiBody({ type: UserSignupDto })
+    @ApiUnauthorizedResponse(
+        {
+            description: 'User already exists',
+            schema: {
+                example: {
+                    statusCode: 401,
+                    message: 'User already exists',
+                },
+            }
+        }
+    )
+    @ApiOkResponse(
+        {
+            description: 'User signed up successfully'
+        }
+    )
+    @ApiBadRequestResponse(
+        {
+            description: 'password must contain only letters, numbers and underscores and be between 6 and 30 characters long',
+            schema: {
+                example: {
+                    statusCode: 400,
+                    message: 'password must contain only letters, numbers and underscores and be between 6 and 30 characters long',
+                },
+            }
+        }
+    )
     @HttpCode(HttpStatus.CREATED)
     @Post('signup')
     async signUp(@Body() user: UserSignupDto) {
@@ -23,6 +98,23 @@ export class AuthController {
         return Promise.resolve();
     }
 
+    @ApiBody({ type: UserRevocationDto })
+    @ApiNotFoundResponse(
+        {
+            description: 'User does not exist',
+            schema: {
+                example: {
+                    statusCode: 404,
+                    message: 'User does not exist',
+                },
+            }
+        }
+    )
+    @ApiOkResponse(
+        {
+            description: 'User revoked successfully'
+        }
+    )
     @HttpCode(HttpStatus.OK)
     @Delete('revocation')
     revoke(@Body() user: UserRevocationDto) {
