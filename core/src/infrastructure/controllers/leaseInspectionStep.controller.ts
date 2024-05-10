73c5de61c858befc22d5de13299f8cb7dda6610a
Charles_Bournonville <charles25.bournonville@gmail.com>
2024-05-10 17:42:35 +0200

    feat: add infos for swagger

--- core/src/infrastructure/controllers/leaseInspectionStep.controller.ts
+++ core/src/infrastructure/controllers/leaseInspectionStep.controller.ts
@@ -1,30 +1,119 @@
 import { Body, Controller, Get, HttpCode, HttpStatus, Param, ParseUUIDPipe, Post, Put } from '@nestjs/common';
 import { LeaseInspectionStepDto } from '@infrastructure/dtos/leaseInspectionStep.dto';
 import { LeaseInspectionStepService } from '@domain/services/leaseInspectionStep.service';
-  
+import { ApiBody, ApiCreatedResponse, ApiNotFoundResponse, ApiOkResponse, ApiParam, ApiTags } from '@nestjs/swagger';
+
+
+@ApiTags('lease_inspection/step')
 @Controller('lease_inspection/step')
 export class LeaseInspectionStepController {
 
     constructor(private service: LeaseInspectionStepService) {}
 
+    @ApiBody({ 
+        type: LeaseInspectionStepDto,
+        description: 'The lease inspection step to create',
+        required: true
+    })
+    @ApiCreatedResponse({ 
+        description: 'The lease inspection step has been successfully created' ,
+        type: LeaseInspectionStepDto
+    })
+    @ApiNotFoundResponse({
+        description: 'The lease inspection step does not exist',
+        schema : {
+            example: {
+                message: 'The lease inspection step does not exist',
+                statusCode: 404
+            }
+        }
+    })
     @HttpCode(HttpStatus.CREATED)
     @Post()
     create(@Body() leaseInspectionStepDto : LeaseInspectionStepDto) {
         return this.service.create(leaseInspectionStepDto);
     }
 
+    @ApiParam({
+        name: 'id',
+        type: 'string',
+        description: 'The id of the lease inspection step',
+        format: 'uuid',
+        example: '123e4567-e89b-12d3-a456-426614174000',
+        required: true,
+    })
+    @ApiOkResponse({
+        description: 'The lease inspection step has been successfully found',
+        type: LeaseInspectionStepDto
+    })
+    @ApiNotFoundResponse({
+        description: 'The lease inspection step does not exist',
+        schema: {
+            example: {
+                message: 'The lease inspection step does not exist',
+                statusCode: 404
+            }
+        }
+    })
     @HttpCode(HttpStatus.OK)
     @Get(':id')
     get(@Param('id', ParseUUIDPipe) id: string) {
         return this.service.get(id);
     }
 
+    @ApiParam({
+        name: 'id',
+        type: 'string',
+        description: 'The id of the lease inspection step',
+        format: 'uuid',
+        example: '123e4567-e89b-12d3-a456-426614174000',
+        required: true,
+    })
+    @ApiBody({
+        type: LeaseInspectionStepDto,
+        description: 'The lease inspection step to update',
+        required: true
+    })
+    @ApiOkResponse({
+        description: 'The lease inspection step has been successfully updated',
+        type: LeaseInspectionStepDto
+    })
+    @ApiNotFoundResponse({
+        description: 'The lease inspection step does not exist',
+        schema: {
+            example: {
+                message: 'The lease inspection step does not exist',
+                statusCode: 404
+            }
+        }
+    })
     @HttpCode(HttpStatus.OK)
     @Put(':id')
     update(@Param('id', ParseUUIDPipe) id: string, @Body() leaseInspectionStepDto : LeaseInspectionStepDto) {
         return this.service.update(id, leaseInspectionStepDto);
     }
 
+    @ApiParam({
+        name: 'leaseId',
+        type: 'string',
+        description: 'The id of the lease inspection',
+        format: 'uuid',
+        example: '123e4567-e89b-12d3-a456-426614174000',
+        required: true,
+    })
+    @ApiOkResponse({
+        description: 'The lease inspection step has been successfully found',
+        type: LeaseInspectionStepDto
+    })
+    @ApiNotFoundResponse({
+        description: 'The lease inspection step does not exist',
+        schema: {
+            example: {
+                message: 'The lease inspection step does not exist',
+                statusCode: 404
+            }
+        }
+    })
     @HttpCode(HttpStatus.OK)
     @Get("lease_inspection/:leaseId")
     getByLeaseInspetion(@Param('leaseId', ParseUUIDPipe) leaseId: string) {
--- core/src/infrastructure/dtos/leaseInspectionStep.dto.ts
+++ core/src/infrastructure/dtos/leaseInspectionStep.dto.ts
@@ -5,23 +5,47 @@ import {  LeaseInspectionStepStateDto } from "./enum/leaseInspectionStep.enum.dt
 export class LeaseInspectionStepDto {
     @IsOptional()
     @IsUUID(4, { message: 'id must be a valid uuid' })
-    @ApiProperty()
+    @ApiProperty({
+        type: 'string',
+        description: 'The id of the lease inspection step',
+        example: '123e4567-e89b-12d3-a456-426614174000',
+    })
     id?: string;
 
     @IsEnum(LeaseInspectionStepStateDto, { message: 'state must be a valid type' })
-    @ApiProperty()
+    @ApiProperty({
+        type: 'string',
+        description: 'The state of the lease inspection step',
+        example: 'PENDING',
+        required: true,
+        enum: LeaseInspectionStepStateDto
+    })
     state: LeaseInspectionStepStateDto;
 
     @IsString({ message: 'description must be a valid string' })
-    @ApiProperty()
+    @ApiProperty({
+        type: 'string',
+        description: 'The description of the lease inspection step',
+        example: 'The kitchen is in a bad state',
+        required: true,
+    })
     description: string;
 
     @IsNumber({}, { message: 'rating must be a valid number' })
-    @ApiProperty()
+    @ApiProperty({
+        type: 'number',
+        description: 'The rating of the lease inspection step',
+        example: 3,
+        required: true,
+    })
     rating: number;
 
     @IsUUID(4, { message: 'leaseInspectionId must be a valid uuid' })
-    @ApiProperty()
+    @ApiProperty({
+        type: 'string',
+        description: 'The id of the lease inspection',
+        example: '123e4567-e89b-12d3-a456-426614174000',
+    })
     leaseInspectionId: string;
 
     constructor(state: LeaseInspectionStepStateDto, description: string, rating: number, leaseInspectionId: string, id?: string) {
