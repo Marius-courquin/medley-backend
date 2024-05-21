61b194f33512d495c2f4d82a96f2283527a70ca7
Marius Courquin <78900916+Marius-courquin@users.noreply.github.com>
2024-05-21 19:10:12 +0200

    feat(lease): get by estate id
    * rebase
    
    * rebase
    
    * rebase
    
    * feat(lease): get by estate id
    
    ---------
    
    Co-authored-by: Charles_Bournonville <charles25.bournonville@gmail.com>

--- core/src/domain/repositories/lease.repository.ts
+++ core/src/domain/repositories/lease.repository.ts
@@ -23,11 +23,23 @@ export class LeaseRepository extends Repository<Lease>{
 
     async findByTenant(tenantId: string): Promise<Lease[]> {
         return this.createQueryBuilder("lease")
-                    .leftJoinAndSelect("lease.tenant", "tenant")
-                    .leftJoinAndSelect("lease.agent", "agent")
-                    .leftJoinAndSelect("lease.estate", "estate")
-                    .where("tenant.id = :tenantId", { tenantId })
-                    .getMany();
+            .leftJoinAndSelect("lease.tenant", "tenant")
+            .leftJoinAndSelect("lease.agent", "agent")
+            .leftJoinAndSelect("lease.estate", "estate")
+            .where("tenant.id = :tenantId", { tenantId })
+            .getMany();
+    }
+
+    async findByEstate(estateId: string): Promise<Lease> {
+        const today = new Date().toISOString().split('T')[0];
+
+        return this.createQueryBuilder("lease")
+            .leftJoinAndSelect("lease.tenant", "tenant")
+            .leftJoinAndSelect("lease.agent", "agent")
+            .leftJoinAndSelect("lease.estate", "estate")
+            .where("estate.id = :estateId", { estateId })
+            .andWhere("lease.start_date <= :today AND lease.end_date >= :today", { today })
+            .getOne();
     }
 
     async findById(id: string): Promise<Lease | undefined> {
